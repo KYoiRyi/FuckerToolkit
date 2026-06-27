40c732c08c572f26fe674aaf23241eb39ef098bb
KYoiRyi <KYoiRyi@users.noreply.github.com>
2026-06-27 21:47:22 +0800

    Retain exported entry symbols

--- src/ftk.zig
+++ src/ftk.zig
@@ -7,6 +7,16 @@ pub const memory = @import("memory.zig");
 pub const pal = @import("pal.zig");
 pub const vfs = @import("vfs.zig");
 
+comptime {
+    _ = bootstrap.ftk_bootstrap_run_once;
+    _ = entry.DllMain;
+    _ = entry.JNI_OnLoad;
+    _ = entry.ftk_platform_constructor_entry;
+    _ = hook.ftk_hook_attach;
+    _ = hook.ftk_hook_detach;
+    _ = logger.ftk_log_write;
+}
+
 test {
     _ = pal;
     _ = vfs;
